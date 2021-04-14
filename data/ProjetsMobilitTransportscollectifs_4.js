32f7e47ed6b3924027f3704b0e4da6cb252cfbbc
dgcl-sdcat <80058755+dgcl-sdcat@users.noreply.github.com>
2021-04-14 15:24:47 +0200

    Add files via upload

---
+++ data/ProjetsMobilitTransportscollectifs_4.js
@@ -0,0 +1,9 @@
+var json_ProjetsMobilitTransportscollectifs_4 = {
+"type": "FeatureCollection",
+"name": "ProjetsMobilitTransportscollectifs_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Thème": "Mobilités du quotidien", "Projets": "Matériel routier à faible émission et station d'avitaillement GNV - RDTA", "Coût": "7 517 925 €", "Subvention": "6 014 340 €" }, "geometry": { "type": "Point", "coordinates": [ 6.197231445, 48.69326921 ] } },
+{ "type": "Feature", "properties": { "Thème": "Mobilités du quotidien", "Projets": "Matériel routier à faible émission et station d'avitaillement GNV - SPL CTBR", "Coût": "9 680 525 €", "Subvention": "7 744 420 €" }, "geometry": { "type": "Point", "coordinates": [ 6.196796925, 48.69305676 ] } }
+]
+}
